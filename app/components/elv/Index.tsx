7265456653ec244e1b25e5c1553549c5c7512be2
jishnum007 <jishnu@globalsurf.ae>
2026-07-01 14:37:52 +0530

    biuld fix

--- app/components/elv/Index.tsx
+++ app/components/elv/Index.tsx
@@ -1,41 +1,9 @@
  
- import Banner from "./Sections/Banner" ;
- import TrustedBy from "./Sections/TrustedBy";
- import UnderstandingAmc from "./Sections/UnderstandingAmc";
- import TheRightFit from "./Sections/TheRightFit";
- import GsitDifference from "./Sections/GsitDifference";
- import ItAmcCost from "./Sections/ItAmcCost";
- import ServiceLevelAgreement from "./Sections/ServiceLevelAgreement";
- import OurCoverageTypes from './Sections/OurCoverageTypes';
- import IndustriesWeServe from './Sections/IndustriesWeServe';
- import SeeItAnAction from './Sections/SeeItAnAction';
- import EscalationFlow from './Sections/EscalationFlow' ;
- import Testimonials  from "../home/Sections/Testimonials";
-import { amcTestimonialsHeader, faqHeaderData, faqData } from "./data"; 
-import{testimonialsData}  from "@/app/components/common/data";
-import GettingStarted from './Sections/GettingStarted';
-import ITSecurityKnowledge from './Sections/ITSecurityKnowledge';
-import FaqSection from  '@/app/components/common/Faq/FaqSection';
-import RelatedServices from './Sections/RelatedServices';
+ 
 const Index = () => {
   return (
     <>
-    <Banner /> 
-    <TrustedBy />
-    <UnderstandingAmc />
-    <TheRightFit />
-    <GsitDifference />
-    <ItAmcCost />
-    <ServiceLevelAgreement />
-    <OurCoverageTypes />
-    <IndustriesWeServe />
-    <SeeItAnAction />
-    <EscalationFlow />
-    <GettingStarted /> 
-    <Testimonials data={testimonialsData} header={amcTestimonialsHeader} />
-    <ITSecurityKnowledge />
-    <FaqSection faqHeaderData={faqHeaderData}  faqData={faqData}/>
-    <RelatedServices />
+    dd
     </>
   );
 };
